071973f08521b25540454e209ad97bbf7042d5d3
LYin <2113635977@qq.com>
2025-09-03 10:04:58 +0800

    msg

--- Flower/pytorchl/Tensor.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torch.xpu import device
-
-device = torch.device('cuda'if torch.cuda.is_available() else 'cpu')
-print(torch.cuda.is_available())
-tensor = torch.tensor([1,2,3],device=device)
-
-print(tensor.shape)
-print(tensor.device)
